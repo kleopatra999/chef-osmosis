a52ae9178fc8c5a645370bffe6cd50c1c333f5de
Grant Heffernan <heffergm@gmail.com>
2013-09-11 15:37:28 -0400

    1

---
+++ recipes/build.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: osmosis
+# Recipe:: build
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'git'
+
+directory '/opt/osmosis' do
+  action :create
+  owner 'root'
+  group 'root'
+  mode 0755
+end
+
+bash 'wget_osmosis' do
+  action :nothing
+  cwd '/tmp'
+  code <<-EOH
+    wget http://bretth.dev.openstreetmap.org/osmosis-build/osmosis-latest.tgz
+    tar zxf osmosis/osmosis-latest.tgz -C /opt/osmosis
+  EOH
+end
+
---
+++ recipes/packages.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: osmosis
+# Recipe:: packages
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'apt'
+
+%w(dh-make openjdk-7-jre).each do |p|
+  package p do
+    action :install
+  end
+end
+
