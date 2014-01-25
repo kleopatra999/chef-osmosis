81bd3236a9b9e8df4952c32fffbf827c713db92b
Grant Heffernan <heffergm@gmail.com>
2014-01-25 13:30:30 -0500

    only update if changed

--- recipes/build.rb
+++ recipes/build.rb
@@ -13,7 +13,18 @@ directory node[:osmosis][:installdir] do
   mode    node[:osmosis][:dirmode]
 end
 
+http_request "HEAD #{node[:osmosis][:remote_source]}" do
+  message ""
+  url node[:osmosis][:remote_source]
+  action :head
+  if File.exists?("#{node[:osmosis][:installdir]}/#{node[:osmosis][:filename]}")
+    headers "If-Modified-Since" => File.mtime("#{node[:osmosis][:installdir]}/#{node[:osmosis][:filename]}").httpdate
+  end
+  notifies :create, "remote_file[#{node[:osmosis][:installdir]}/#{node[:osmosis][:filename]}]", :immediately
+end
+
 remote_file "#{node[:osmosis][:installdir]}/#{node[:osmosis][:filename]}" do
+  action    :nothing
   source    node[:osmosis][:remote_source]
   notifies  :run, 'bash[extract-osmosis]', :immediately
 end
