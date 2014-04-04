332349123468e46c19263c4dc9c3ada0d2f604a6
Grant Heffernan <heffergm@gmail.com>
2014-04-04 16:30:35 -0400

    attribute comments

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,11 +2,15 @@
 #
 
 default[:osmosis][:install_type]  = 'pkg' # 'pkg' or 'tgz'
+
+# for pkg
+#
 default[:osmosis][:pkg_version]   = '0.0.1'
 default[:osmosis][:user]          = 'root'
 default[:osmosis][:group]         = 'root'
 
-# for pkg install
+# for tgz
+#
 default[:osmosis][:installdir]    = '/opt/osmosis'
 default[:osmosis][:dirmode]       = 0755
 default[:osmosis][:remote_source] = 'http://bretth.dev.openstreetmap.org/osmosis-build/osmosis-latest.tgz'
