029df8c5a26beaa9b77079fa5ef7fdfcd5e2a839
Grant Heffernan <heffergm@gmail.com>
2014-07-18 09:51:38 -0400

    clean up logic

--- recipes/install.rb
+++ recipes/install.rb
@@ -3,18 +3,17 @@
 # Recipe:: install
 #
 
-case node[:osmosis][:install_type]
-when 'tgz'
-  ark 'osmosis' do
-    url               node[:osmosis][:url]
-    version           node[:osmosis][:version]
-    owner             node[:osmosis][:user]
-    prefix_root       node[:osmosis][:installdir]
-    has_binaries      ['bin/osmosis']
-    strip_components  0
-  end
-when 'pkg'
-  package 'osmosis' do
-    action :install
-  end
+ark 'osmosis' do
+  url               node[:osmosis][:url]
+  version           node[:osmosis][:version]
+  owner             node[:osmosis][:user]
+  prefix_root       node[:osmosis][:installdir]
+  has_binaries      ['bin/osmosis']
+  strip_components  0
+  only_if { node[:osmosis][:install_type] == 'tgz' }
+end
+
+package 'osmosis' do
+  action :install
+  only_if { node[:osmosis][:install_type] == 'pkg' }
 end
